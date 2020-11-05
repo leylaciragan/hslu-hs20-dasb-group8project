9b3e0b2860cc12b2611eec1e8e0026e04302fea5
Ciragan Leyla lciragan <leyla.ciragan@stud.hslu.ch>
2020-11-05 12:13:20 +0100

    added color palettes for gdp and population, implemented multiline labels, added tab 7: about this project

--- app.R
+++ app.R
@@ -105,24 +105,78 @@ ui <- fluidPage(
   
   # Tab 6
   tabPanel(
-    "Data",
-    "On this page you can check the original data used in this project.",
+    "Data and Data Cleaning",
+    "The following data was used in our project:",
+    tags$br(),
+    tags$br(),
+    tags$b("1. Asylgesuche nach Nationen (1986 bis 2020). "), 
+    "Data published by ",
+    tags$a(href="https://www.sem.admin.ch/sem/fr/home/publiservice/statistik/asylstatistik/uebersichten.html",
+           "Federal statistical office of Switzerland."),
+    tags$p("To this data set we added a column with the countries postal codes, so we could map the data with other datasets."),
+    tags$br(),
+    tags$br(),
     DTOutput(outputId = "datatable"),
     verbatimTextOutput("rawtable"),
     downloadButton("downloadCsv", 
                    "Download as CSV"),
     tags$br(),
     tags$br(),
+    tags$b("2. countries.geo.json. "), 
     "Data published by ",
-    tags$a(href="https://www.sem.admin.ch/sem/fr/home/publiservice/statistik/asylstatistik/uebersichten.html",
-           "Federal statistical office of Switzerland.")
-           )
+    tags$a(href="https://github.com/johan/world.geo.json/blob/master/countries.geo.json",
+           "Johan on GitHub."),
+    tags$p("We cleaned out all redundant and unnecessary data from this dataset (e.g. redundant country names, codes, etc."),
+    tags$br(),
+    tags$br()
+           ), # end tab 6
+  
+  # Tab 7
+  tabPanel(
+    "About this project", 
+    
+    # Dear team, feel free to change the texts, I'm feeling a little cheesy today (Leyla)
+    tags$p("With COVID19 and US elections predominant in the media in 2020 we asked ourselves, what had happened to the refugees in this world. After camp Moria burning down in autumn 2020, we decided to focus our attention to this 'forgotten' topic."),
+    tags$p("Narrowing down the area of our research question, we looked for numbers on asylum seekers to Switzerland. With datasets on asylum seekers in Switzerland and others on world conflicts, we try to visualize the bare numbers, but also to investigate possible conncections."),
+    tags$p(
+    tags$b("Contributors:"),
+    tags$li("Leyla, Ciragan"),
+    tags$li("Tariq Ghazzawi"),
+    tags$li("Lukas Ingold"),
+    tags$li("Gabriela Moos"),
+    tags$li("Urs Stadelmann")
+    ),
+    tags$h4("We will not forget."), 
+    img(src = "moria.jpeg", width = "844px", height = "475px"),
+    tags$legend("Migrants flee the flames at Moria camp | Photo: Picture-alliance/dpa/S.Baltagiannis", 
+                tags$a(href="https://www.infomigrants.net/en/post/27165/fresh-fires-burn-at-greece-s-largest-refugee-camp-moria")
+                )
+  ) # end tab 7
   ) # end navbarPage
   ) # end fluidPage
 
 
 # Define server function  
 server <- function(input, output) {
+  
+  # color palette
+  pal <- colorNumeric(
+    palette = "YlGnBu", # other palettes for example: GnBu, OrRd, YlOrBr, see https://colorbrewer2.org/#type=sequential&scheme=YlGnBu&n=5
+    domain = countries$pop_est)
+  
+  pal2 <- colorNumeric(
+    palette = "GnBu", # other palettes for example: GnBu, OrRd, YlOrBr, see https://colorbrewer2.org/#type=sequential&scheme=YlGnBu&n=5
+    domain = countries$gdp_md_est)
+  
+  labels <- sprintf(
+    "<strong>Country: %s</strong><br/><br/>
+    Postal Code: %s<br/>
+    Population: %g<br/>
+    GDP: %g<br/>
+    Economical Status: %s",
+    countries$formal_en, countries$postal, countries$pop_est, countries$gdp_md_est, countries$economy
+  ) %>% 
+    lapply(htmltools::HTML)
   
   # output tab 1: map with Leaflet
   output$map <- renderLeaflet({
@@ -135,19 +189,31 @@ server <- function(input, output) {
                        options = providerTileOptions(minzoom = 1, maxzoom = 10)) %>%
       addProviderTiles("Esri.WorldTopoMap",    
                        group = "Topo") %>% 
-      # Overlay groups for map
+      
+
+      # Overlay groups for map$
+      # TODO add an overlay for number of applications
       addPolygons(
-        group = "Countries", 
+        group = "Countries: Population", 
         data = countries, 
-        label = ~htmlEscape(formal_en), # TODO add some styling and more fields from countries.json like: pop_est, gdp_est, economy, income_grp
+        label = labels, 
         weight = 1, 
         color = "White", 
         opacity = 1, 
-        fillColor = "Red", # TODO should be depending on number of applications
-        fillOpacity = 0.1) %>%
+        fillColor = ~pal(countries$pop_est), 
+        fillOpacity = 0.7) %>%
+      addPolygons(
+        group = "Countries: GDP", 
+        data = countries, 
+        label = labels, 
+        weight = 1, 
+        color = "White", 
+        opacity = 1, 
+        fillColor = ~pal2(countries$gdp_md_est), 
+        fillOpacity = 0.7) %>%
       addLayersControl(
         baseGroups = c("OSM", "Toner", "Topo"),
-        overlayGroups = c("Countries"),
+        overlayGroups = c("Countries: Population", "Countries: GDP"),
         options = layersControlOptions(collapsed = FALSE))
   })
   
@@ -159,15 +225,11 @@ server <- function(input, output) {
   
   # TODO: output tab 5: top & bottom countries
   
-    # TODO what is this doing?
-  # output$txtout <- renderText({
-  #   paste( input$txt1, input$txt2, sep = " " )
-  # })
-  
+
   # output tab 6: data table with DT
   output$datatable <- renderDT(applications)
   
-  # output tab 6: download data as csv: TODO does this work?
+  # output tab 6: download data as csv: TODO is this from an example? Who fixes it?
   output$downloadCsv <- downloadHandler(
     filename = function() {
       paste("refugee_data", applications$date[1], ".csv", sep="")
@@ -177,12 +239,6 @@ server <- function(input, output) {
     }
   )
 
-  # TODO keep either this table (not working yet) or datatable with renderDT
-  # output$rawtable <- renderPrint({
-  # orig <- options(width = 1000)
-  # print(tail(applications %>% select(c(Country, 1986, 1987)), input$maxrows), row.names = FALSE)
-  # options(orig)
-  # })
 } # end server
 
 
