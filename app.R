260a5811fb8d7d11b821aa5476099b2934cbb3f4
Lukas Ingold <technik@ingold.li>
2020-12-03 16:52:40 +0100

    removed colnames

--- app.R
+++ app.R
@@ -383,7 +383,6 @@ server <- function(input, output) {
   output$top10 <- renderTable({
     # edit the dataset to fit the table
     A <- applications
-    colnames(A) <- c("Code","Country","1986","1987","1988","1989","1990","1991","1992","1993","1994","1995","1996","1997","1998","1999","2000","2001","2002","2003","2004","2005","2006","2007","2008","2009","2010","2011","2012","2013","2014","2015","2016","2017","2018","2019","2020")
     A[A$Code  == "Stateless", "Country"]<- "Stateless"
     A[A$Code  == "Without Nationality", "Country"]<- "Without Nationality"
     A[A$Code  == "State unknown", "Country"]<- "State unknown"
