b7a04ca2e330840ad37e86eab67403548c1b50d5
ursstadelmann <urs.stadelmann@gmail.com>
2020-12-09 20:32:56 +0100

    added heatmap

--- app.R
+++ app.R
@@ -12,7 +12,7 @@ library(dplyr)
 library(magrittr)
 library(data.table)
 library(ggplot2)
-
+library(reshape2)
 
 # import data. Disable 'check.names' to prevent shiny from adding an X prefix.
 applications <- read.csv("data/AsylgesuchePerNation1986.csv", sep=";", encoding="UTF-8",  header = TRUE, check.names = FALSE)
@@ -21,7 +21,7 @@ applis_trnspsd <- read.csv("data/appli_transposed.csv", sep=";", encoding="UTF-8
 # happiness is used for tab 3
 happiness_combined <- read.csv("data/happy_transposed.csv", sep=";", encoding="UTF-8", header=TRUE, check.names = FALSE)
 happy_countries <- read.csv("data/Happiness/combined_cleaned.csv", sep=",", encoding="UTF-8", header=TRUE, check.names = FALSE)
-happiness_correlation <- read.csv("data/Happiness/Correlation/happiness_correlation2.csv", sep=";", encoding="UTF-8", header = TRUE, check.names = FALSE)
+happiness_correlation <- read.csv("/Users/urs/Festplatte/03_HSLU/00_exercises/dasb_team08/data/Happiness/Correlation/Useable/CSV_File_2019.csv", sep=",", encoding="UTF-8", header = TRUE, check.names = FALSE)
 
 # Countries and map are used on map tab
 countries <- geojson_read("data/countries.geo.json", what = "sp") # for country polygons, gdp_md_est and pop_est
@@ -86,27 +86,27 @@ ui <- fluidPage(
   ### START: Tab 3
   tabPanel(
     "Correlation of migration and happiness",
-    "See how happiness influences migration.",
+    "See how happiness-factors influence migration.",
     tags$br(),
     tags$br(),
     tags$br(),
     tags$br(),
     
-    sidebarLayout(
-      sidebarPanel(
-        
+      mainPanel(
+        #plot a heatmap
+        plotOutput(outputId = "correlation_heat_map_numbered"),
+      
         # Select country of origin to plot
         selectInput(inputId = "Country2", 
                     label = "Choose country", 
                     c(sort(happy_countries$Country)), 
                     selected = "Afghanistan", multiple = FALSE),
-      ),
-      mainPanel(
+        
+        #plotOutput as dygraph
         dygraphOutput(outputId = "timetrend2"),
         #plotOutput as dygraph
-        dygraphOutput(outputId = "happinesstrend")
-      )
-    )
+        dygraphOutput(outputId = "happinesstrend"),
+      ),
   ),
   ### END: Tab 3
   
@@ -379,6 +379,85 @@ server <- function(input, output) {
     dygraph(selected_countries2(), main = "No. of applications from 2016-2019") %>%
       dyRangeSelector(height = 20) %>%
       dyOptions(colors = RColorBrewer::brewer.pal(9, "Set1"))
+  })
+  
+  #correlation 
+  output$correlation_heat_map_numbered <- renderPlot({
+  cor_data <- happiness_correlation[,3:length(happiness_correlation)]
+  cormat <- round(cor(cor_data),2)
+  
+  # Get lower triangle of the correlation matrix
+  get_lower_tri<-function(cormat){
+    cormat[upper.tri(cormat)] <- NA
+    return(cormat)
+  }
+  
+  # Get upper triangle of the correlation matrix
+  get_upper_tri <- function(cormat){
+    cormat[lower.tri(cormat)]<- NA
+    return(cormat)
+  }
+  
+  upper_tri <- get_upper_tri(cormat)
+  upper_tri
+  
+  # Melt the correlation matrix
+  library(reshape2)
+  melted_cormat <- melt(upper_tri, na.rm = TRUE)
+  # Heatmap
+  library(ggplot2)
+  ggplot(data = melted_cormat, aes(Var2, Var1, fill = value))+
+    geom_tile(color = "white")+
+    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                         midpoint = 0, limit = c(-1,1), space = "Lab", 
+                         name="Pearson\nCorrelation") +
+    theme_minimal()+ 
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                     size = 12, hjust = 1))+
+    coord_fixed()
+  
+  
+  reorder_cormat <- function(cormat){
+    # Use correlation between variables as distance
+    dd <- as.dist((1-cormat)/2)
+    hc <- hclust(dd)
+    cormat <-cormat[hc$order, hc$order]
+  }
+  
+  # Reorder the correlation matrix
+  cormat <- reorder_cormat(cormat)
+  upper_tri <- get_upper_tri(cormat)
+  # Melt the correlation matrix
+  melted_cormat <- melt(upper_tri, na.rm = TRUE)
+  # Create a ggheatmap
+  ggheatmap <- ggplot(melted_cormat, aes(Var2, Var1, fill = value))+
+    geom_tile(color = "white")+
+    scale_fill_gradient2(low = "blue", high = "red", mid = "white", 
+                         midpoint = 0, limit = c(-1,1), space = "Lab", 
+                         name="Pearson\nCorrelation") +
+    theme_minimal()+ # minimal theme
+    theme(axis.text.x = element_text(angle = 45, vjust = 1, 
+                                     size = 12, hjust = 1))+
+    coord_fixed()
+  # Print the heatmap
+  correlation_heat_map_numbered <- ggheatmap
+  
+  ggheatmap + 
+    geom_text(aes(Var2, Var1, label = value), color = "black", size = 4) +
+    theme(
+      axis.title.x = element_blank(),
+      axis.title.y = element_blank(),
+      panel.grid.major = element_blank(),
+      panel.border = element_blank(),
+      panel.background = element_blank(),
+      axis.ticks = element_blank(),
+      legend.justification = c(1, 0),
+      legend.position = c(0.6, 0.7),
+      legend.direction = "horizontal")+
+    guides(fill = guide_colorbar(barwidth = 7, barheight = 1,
+                                 title.position = "top", title.hjust = 0.5))
+  
+  
   })
   ### End output tab 3
   
